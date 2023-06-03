1b41634aa3ca94b31b7d682f42a668d4a7051a43
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2023-06-03 10:43:54 +0300

    Setup of index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("Raboti")
+const express = require('express');
+
+const app = express();
+const PORT = 5000;
+
+app.get('/', (req, res) => {
+    res.send('It works')
+});
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
