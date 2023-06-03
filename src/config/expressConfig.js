31aa6294a164aaa437c24a44c4d2d609af5b3503
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2023-06-03 12:15:14 +0300

    Add express and handlebars configuration

---
+++ src/config/expressConfig.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path  = require('path');
+
+function expressConfig(app) {
+    app.use(express.static(path.resolve(__dirname, '../public')));
+    app.use(express.urlencoded({extended : false}));
+    // app.use(express.static('src/public'));
+};
+
+module.exports = expressConfig;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
 const express = require('express');
-const handlebars = require('express-handlebars');
 
 const app = express();
 const PORT = 5000;
 
 //Express config
-//Middleware
-app.use(express.static('src/public'))
+const expressConfig = require('./config/expressConfig');
+expressConfig(app);
 
 //Handlebars config
-app.engine('hbs', handlebars.engine({
-    extname:'hbs', 
-}));
-app.set('view engine', 'hbs');
-app.set('views', 'src/views'); 
+const handlebarsConfig = require('./config/handlebarsConfig');
+handlebarsConfig(app);
 
 //Routes 
 app.get('/', (req, res) => {
