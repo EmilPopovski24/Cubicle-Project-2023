cd54888702765f78b599b60596cb8981e95596e0
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2023-06-11 12:27:56 +0300

    Add login page

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -11,6 +11,10 @@ router.post('/register', async(req, res) => {
     console.log(req.body);
 
     res.redirect('/users/login')
+});
+
+router.get('/login', (req, res) => {
+    res.render('./users/login')
 })
 
 
