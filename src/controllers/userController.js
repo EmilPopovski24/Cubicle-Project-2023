c71bcd0f393f0feb901003db1598e6428d2c3db2
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2023-06-11 12:07:53 +0300

    Setting register page and its details

---
+++ src/controllers/userController.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const userManager = require('../managers/UserManager');
+
+router.get('/register', (req, res) => {
+    res.render('users/register')
+})
+
+router.post('/register', async(req, res) => {
+    const { username, password, repeatPassword } = req.body;
+    await userManager.register({username, password, repeatPassword});
+    console.log(req.body);
+
+    res.redirect('/users/login')
+})
+
+
+module.exports = router;
